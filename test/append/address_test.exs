acd35ed80ff54de9d8156e3ebb0c3f480c020fd7
nelsonic <nelson+github@dwyl.io>
2019-03-25 03:43:16 +0000

    add failing tests for get/1 and all/0 section 4.2

--- test/append/address_test.exs
+++ test/append/address_test.exs
@@ -14,4 +14,30 @@ defmodule Append.AddressTest do
 
     assert item.name == "Thor"
   end
+
+  describe "get items from database" do
+    test "get/1" do
+      {:ok, item} = insert_address()
+
+      assert Address.get(item.id) == item
+    end
+
+    test "all/0" do
+      {:ok, _item} = insert_address()
+      {:ok, _item_2} = insert_address("Loki")
+
+      assert length(Address.all()) == 2
+    end
+  end
+
+  def insert_address(name \\ "Thor") do
+    Address.insert(%{
+      name: name,
+      address_line_1: "The Hall",
+      address_line_2: "Valhalla",
+      city: "Asgard",
+      postcode: "AS1 3DG",
+      tel: "0800123123"
+    })
+  end
 end
