d2f02e1d1afc88d4c84899c0dcd525ab56f1f0d7
nelsonic <nelson+github@dwyl.io>
2019-03-25 04:35:24 +0000

    seciont 4.3 add entri_id to address.ex #1

--- lib/append/address.ex
+++ lib/append/address.ex
@@ -9,6 +9,7 @@ defmodule Append.Address do
     field :address_line_1, :string
     field :address_line_2, :string
     field :city, :string
+    field(:entry_id, :string)
     field :name, :string
     field :postcode, :string
     field :tel, :string
@@ -19,7 +20,15 @@ defmodule Append.Address do
   @doc false
   def changeset(address, attrs) do
     address
-    |> cast(attrs, [:name, :address_line_1, :address_line_2, :city, :postcode, :tel])
-    |> validate_required([:name, :address_line_1, :address_line_2, :city, :postcode, :tel])
+    |> insert_entry_id()
+    |> cast(attrs, [:name, :address_line_1, :address_line_2, :city, :postcode, :tel, :entry_id])
+    |> validate_required([:name, :address_line_1, :address_line_2, :city, :postcode, :tel, :entry_id])
+  end
+
+  def insert_entry_id(address) do
+    case Map.fetch(address, :entry_id) do
+      {:ok, nil} -> %{address | entry_id: Ecto.UUID.generate()}
+      _ -> address
+    end
   end
 end
